8a6db1042f0d316bc533b75354bfd9926fb1109f
Lucas Boucher <lucas@source.paris>
2025-11-04 16:45:32 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', background: 'blue', color: 'white' }}>
+      <h1>TEST MANUEL - PROJET A</h1>
+      <p>Si vous voyez ceci, le repo swap a fonctionné.</p>
+    </div>
+  )
+}
+export default App
